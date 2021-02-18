d91184a655397bc6ef9c30515bbf43cc31037161
Doru Moraru <doru.moraru@gmail.com>
2021-02-18 10:48:38 +0000

    show window content only when actually ready

--- public/electron.js
+++ public/electron.js
@@ -18,20 +18,27 @@ if (!singleInstanceLock) {
       mainWindow.focus()
     }
   })
-
-  // Create myWindow, load the rest of the app, etc...
-  app.on('ready', () => {
-  })
 }
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({ width: 400, height: 300 })
+  mainWindow = new BrowserWindow({ 
+    width: 400, 
+    height: 300,
+    maximizable: false,
+    backgroundColor: '#282C34',
+    show: false,
+  })
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   )
-  mainWindow.on("closed", () => (mainWindow = null))
+  mainWindow.on("closed", () => {
+    mainWindow = null
+  })
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+})
   if (isDev) {
     // Open Console in development mode
     mainWindow.webContents.openDevTools()
