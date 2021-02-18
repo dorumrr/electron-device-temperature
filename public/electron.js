f9361832848d1028529528e039acb4423c80a5e0
Doru Moraru <doru.moraru@gmail.com>
2021-02-18 10:32:49 +0000

    don't allow multiple instances

--- public/electron.js
+++ public/electron.js
@@ -3,8 +3,26 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const path = require("path")
 const isDev = require("electron-is-dev")
+const singleInstanceLock = app.requestSingleInstanceLock()
+let mainWindow
 
-let mainWindow;
+if (!singleInstanceLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Do not open a second instance, focus our mainWindow
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore()
+      }
+      mainWindow.focus()
+    }
+  })
+
+  // Create myWindow, load the rest of the app, etc...
+  app.on('ready', () => {
+  })
+}
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({ width: 400, height: 300 })
@@ -16,14 +34,16 @@ const createWindow = () => {
   mainWindow.on("closed", () => (mainWindow = null))
 }
 
-app.on("ready", createWindow);
+app.on("ready", createWindow)
+
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
-    app.quit();
+    app.quit()
   }
-});
+})
+
 app.on("activate", () => {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
